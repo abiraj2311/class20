8f5b2880196958074de114b8d475d78ee1045eb7
abiraj2311 <74949213+abiraj2311@users.noreply.github.com>
2021-01-05 15:00:44 +0530

    classs 20

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 200, 50, 50);
+  fixedRect.shapeColor="red";
+  movingRect = createSprite(400,100, 90,50);
+  movingRect.shapeColor = "red";
+  console.log(fixedRect.x-movingRect.x);
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+ if(movingRect.x -fixedRect.x <= fixedRect.width/2 + movingRect.width/2 &&
+ fixedRect.x - movingRect.x <= fixedRect.width/2 + movingRect.width/2 &&
+ movingRect.y -fixedRect.y <= fixedRect.height/2 + movingRect.height/2 &&
+ fixedRect.y - movingRect.y  <= fixedRect.height/2 + movingRect.height/2){
+  movingRect.shapeColor = "green";
+  fixedRect.shapeColor="green";
+ }
+  else{
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor="red";
+  }
+
+  drawSprites();
+}
+
